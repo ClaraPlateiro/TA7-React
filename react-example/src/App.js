75b2ae67f9052ca4daf38a81d6b8ed5cbe4736b6
ClaraPlateiro <clara.plateiro@correo.ucu.edu.uy>
2024-09-22 22:51:26 -0300

    TA7 done

---
+++ react-example/src/App.js
@@ -0,0 +1,38 @@
+import {useState} from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask('');
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = tasks.filter((task, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  return (
+    <div>
+      <h1>Lista</h1>
+      <input
+        type="text"
+        value={task}
+        placeholder="Agregar tarea"
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Agregar tarea</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+          {task} <button onClick={() => deleteTask(index)}>Eliminar</button>
+        </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
